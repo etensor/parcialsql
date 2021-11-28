40ef618ff8ff3f5364d0e305bdc3bfc62ac5b60b
etensor <dpenillac@correo.usbcali.edu.co>
2021-11-27 19:48:47 -0500

    mi nota vale profe

--- parcialc3.py
+++ parcialc3.py
@@ -1,3 +1,5 @@
+from attr import s
+from numpy import e
 import psycopg2
 import streamlit as st
 import pandas as pd
@@ -25,95 +27,17 @@ def exec_query(query, mod=True):
 
 
 st.title('Parcial Corte 3: postgreSQL')
-st.subheader('''----------------------------------------------------------------\n
-reconsidere mi nota profe porfavor...''')
-
-
-
-clase_trig, paliza_trig, partida_trig = st.columns(3)
-st.text('Triggers implementados: ')
-with clase_trig:
-    with st.expander('Cambio Rol Jugador',True):
-        st.markdown('''
-        create or replace function change_rol()
-        returns trigger
-        language PLPGSQL as
-        $$
-        begin
-
-            if (new.clase != old.clase ) then
-
-                update jugadores 
-                set roles = array_append(roles,old.clase)
-                where id = new.id;
-            end if; 
-
-            return new;
-        end;
-        $$
-
-        create trigger cambiar_rol
-        after update on jugadores
-        for each row execute procedure change_rol();
-        ''')
-
-with paliza_trig:
-    with st.expander('Una partida desigual', True):
-        st.markdown('''
-        CREATE or REPLACE FUNCTION public.que_paliza() RETURNS trigger
-        LANGUAGE plpgsql
-        AS $$
-        begin 
-
-        if (new.puntajes[1] > 9 and new.puntajes[2] < 2) then 
-            insert into palizas_historicas 
-            values ( new.puntajes[1],now(), new.puntajes[2],
-            cast( array[new.equipo_a,new.equipo_b] as t_equipo[] ),
-            new.id,new.equipo_a); 
-        elsif ( new.puntajes[2] > 9 and new.puntajes[1] < 2 ) then 
-             insert into palizas_historicas(asesinatos_A,fecha,asesinatos_B,equipos,id,ganador) 
-            values ( new.puntajes[1],now(), new.puntajes[2],
-            cast( array[new.equipo_a,new.equipo_b] as t_equipo[] ),
-            new.id,new.equipo_b); 
-
-        end if;
-
-        return new;
-        end;
-        $$; 
-
-
-        create trigger conmemorar_paliza
-        after insert or update
-        on partida
-        for each row execute function que_paliza();
-        ''')
-
-with partida_trig:
-    with st.expander('Creacion Partida: Vinculo Equipos', True):
-        st.markdown('''
-        ```plsql
-        CREATE FUNCTION public.new_partida() RETURNS trigger
-        LANGUAGE plpgsql
-        AS $$
-        begin
-	
-	        insert into equipos(players)
-	        values (new.equipo_a),(new.equipo_b);
-	
-	        new.id_equipo_a = (select id from equipos where players = new.equipo_a);	
-	        new.id_equipo_b = (select id from equipos where players = new.equipo_b);
-
-	    return new;
-        end;
-        ```
-
-        ''')
+st.subheader('''\n
+reconsidere mi nota profe porfavor..., en sus manos está mi materia.''')
 
 
 
 st.subheader('1. Consultas')
-a,b,c,d,e = st.row(5)
+a = st.container()
+b = st.container()
+c = st.container()
+d = st.container()
+e = st.container()
 
 query_a = exec_query('''
 
@@ -160,26 +84,185 @@ select * from jugadores where puntuacion =
 (select max(puntuacion) from jugadores);
 ''')
 
-
 with a:
-    st.dataframe(pd.DataFrame(query_a,
-    columns = {'nombre','correo','premium','clase','id','puntuacion','roles'}))
+    with st.expander('1. ¿Qué jugadores juegan más de utilidad?',True):
+        st.dataframe(pd.DataFrame(query_a,
+        columns = {'nombre','correo','premium','clase','id','puntuacion','roles'}))
+
 
 with b:
-    st.dataframe(pd.DataFrame(query_b,
-    columns = {'equipo_a','equipo_b','id','puntajes','id_equipo_a','id_equipo_b'}))
+    with st.expander('2. Qué partidas tienen jugadores que no juega de defensa',True):
+        st.dataframe(pd.DataFrame(query_b,
+        columns = {'equipo_a','equipo_b','id','puntajes','id_equipo_a','id_equipo_b'}))
 
 with c:
-    st.dataframe(pd.DataFrame(query_c,
-    columns = {'nombre','correo','premium','clase','id','puntuacion','roles'}))
+    with st.expander('3. Qué jugadores juegan en todos los roles que juega el jugador Mario',True):
+        st.dataframe(pd.DataFrame(query_c,
+        columns = {'nombre','correo','premium','clase','id','puntuacion','roles'}))
 
 with d:
-    st.dataframe(pd.DataFrame(query_d,
-    columns = {'tipo_arma','damage','poder_especial','nombre','correo','premium','clase','id','puntuacion','roles'}))
+    with st.expander('4. Qué jugadores juegan más con armas de largo alcance', True):
+        st.dataframe(pd.DataFrame(query_d))
+        #columns = {'tipo_arma','damage','poder_especial','id','nombre','correo','premium','clase','id','puntuacion','roles'}))
 
 with e:
-    st.dataframe(pd.DataFrame(query_e),
-    columns = {'nombre','correo','premium','clase','id','puntuacion','roles'})
+    with st.expander('5. ¿Qué jugadores tienen el puntaje del jugador que más puntos tiene actualmente en la base de datos? ', True):
+        st.dataframe(pd.DataFrame(query_e,
+        columns = {'nombre','correo','premium','clase','id','puntuacion','roles'}))
+
+st.subheader('2. Triggers implementados: ')
+
+clase_trig = st.container()
+paliza_trig = st.container()
+partida_trig = st.container()
 
 
-    
+with clase_trig:
+    with st.expander('Cambio Rol Jugador', True):
+        st.code('''
+        create or replace function change_rol()
+        returns trigger
+        language PLPGSQL as
+        $$
+        begin
+
+            if (new.clase != old.clase ) then
+
+                update jugadores 
+                set roles = array_append(roles,old.clase)
+                where id = new.id;
+            end if; 
+
+            return new;
+        end;
+        $$
+
+        create trigger cambiar_rol
+        after update on jugadores
+        for each row execute procedure change_rol();
+        ''', language='sql')
+
+with paliza_trig:
+    with st.expander('Una partida desigual', True):
+        st.code('''
+        CREATE or REPLACE FUNCTION public.que_paliza() RETURNS trigger
+        LANGUAGE plpgsql
+        AS $$
+        begin 
+
+        if (new.puntajes[1] > 9 and new.puntajes[2] < 2) then 
+            insert into palizas_historicas 
+            values ( new.puntajes[1],now(), new.puntajes[2],
+            cast( array[new.equipo_a,new.equipo_b] as t_equipo[] ),
+            new.id,new.equipo_a); 
+        elsif ( new.puntajes[2] > 9 and new.puntajes[1] < 2 ) then 
+             insert into palizas_historicas(asesinatos_A,fecha,asesinatos_B,equipos,id,ganador) 
+            values ( new.puntajes[1],now(), new.puntajes[2],
+            cast( array[new.equipo_a,new.equipo_b] as t_equipo[] ),
+            new.id,new.equipo_b); 
+
+        end if;
+
+        return new;
+        end;
+        $$; 
+
+
+        create trigger conmemorar_paliza
+        after insert or update
+        on partida
+        for each row execute function que_paliza();
+        ''', language='sql')
+
+with partida_trig:
+    with st.expander('Creacion Partida: Vinculo Equipos', True):
+        st.code('''
+        CREATE FUNCTION public.new_partida() RETURNS trigger
+        LANGUAGE plpgsql
+        AS $$
+        begin
+	
+	        insert into equipos(players)
+	        values (new.equipo_a),(new.equipo_b);
+	
+	        new.id_equipo_a = (select id from equipos where players = new.equipo_a);	
+	        new.id_equipo_b = (select id from equipos where players = new.equipo_b);
+
+	    return new;
+        end;
+
+        ''', language='sql')
+
+with st.container():
+    with st.expander(' Finalización Partida ', True):
+        st.code('''
+        CREATE FUNCTION public.end_partida() RETURNS trigger
+        LANGUAGE plpgsql
+        AS $$
+        declare
+            winner t_equipo;
+        begin
+
+        if (new.puntajes[1] = 10) then
+            winner := new.equipo_a;
+
+            insert into public.registro
+            values (new.puntajes[1],now(),new.puntajes[2],
+                    cast (array[new.equipo_a,new.equipo_b] as t_equipo[]),
+                    default, winner);
+
+        elsif ( new.puntajes[2] = 10 ) then
+            winner := new.equipo_b;
+
+            insert into public.registro
+            values (new.puntajes[1],now(),new.puntajes[2],
+                    cast (array[new.equipo_a,new.equipo_b] as t_equipo[]),
+                    default, winner);
+
+        end if; 
+
+        return new;
+        end;
+        $$;
+
+        CREATE TRIGGER terminar_partida 
+        AFTER INSERT OR UPDATE ON public.partida 
+        FOR EACH ROW EXECUTE FUNCTION public.end_partida();
+    '''
+    ,language='sql')
+
+
+st.subheader('3. Vistas')
+st.write('''
+Crear una vista que consista en los jugadores que nunca 
+hayan jugado una partida, pero que esten registrados 
+en la base de datos. ''')
+
+st.code('''
+create view no_han_jugado as
+select * from jugadores where 
+    nombre not in ( select player1(players) from equipos )
+and nombre not in ( select player2(players) from equipos )
+and nombre not in ( select player3(players) from equipos );
+
+''',language='sql')
+
+
+st.subheader('4. Indices')
+st.write('''
+Yo le aplicaria indices a la tabla jugadores ya que es la principal y van a estar
+ constantemente en partidas. De esta forma se podría acelerar el sistema
+ ya que se trabajaría con los jugadores más activos en la base de datos, que
+ podria ser otra columna, horas de juego recientes.
+
+Otra tabla probablemente extensa y abierta a optimizaciones sería la de partida, ya que puede que haya partidas más activas que otras.
+
+Y si el registro es muy extenso (ya se jugaron muchas partidas), tambien valdría la pena
+
+aplicar un indice ya que podría reorganizar el registro a partir de su fecha y así
+
+volver más rápidas las consultas que la llamen.
+''')
+
+
+st.info('Muchas gracias por su atención.')
